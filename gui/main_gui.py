efd842d0ee07f458e1490595467b715bb97f0f9c
HenriCaboche <henri.caboche.develop@gmail.com>
2025-07-15 21:36:31 +0200

    Finished login stub

--- gui/main_gui.py
+++ gui/main_gui.py
@@ -1,9 +1,13 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QWidget, QLineEdit
+import os
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit, QPushButton
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+stylesheet_path = os.path.join(script_dir, "stylesheet.css")
 
 window = None
 
-with open("stylesheet.css", 'r') as f:
+with open(stylesheet_path, 'r') as f:
     stylesheet = f.read()
 
 def handle_login():
@@ -19,9 +23,12 @@ def handle_login():
     
     email_textbox = QLineEdit(placeholderText="Email")
     password_textbox = QLineEdit(placeholderText= "Password")
-    
+    login_button = QPushButton("Log in")
+
     main_layout.addWidget(email_textbox)
     main_layout.addWidget(password_textbox)
+    main_layout.addWidget(login_button)
+
     main_widget.setLayout(main_layout)
 
     window.setStyleSheet(stylesheet)
