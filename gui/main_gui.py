efaf8832ebce947d621a73fa9e1c03f354a4e945
HenriCaboche <henri.caboche.develop@gmail.com>
2025-07-15 22:37:17 +0200

    Finished login frontend

--- gui/main_gui.py
+++ gui/main_gui.py
@@ -1,18 +1,20 @@
 import sys
 import os
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit, QPushButton
+from typing import Optional
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit, QPushButton, QComboBox, QLabel
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if parent_dir not in sys.path:
     sys.path.insert(0, parent_dir)
 import main
 
-email_textbox = None
-password_textbox = None
+email_textbox: Optional[QLineEdit] = None
+password_textbox: Optional[QLineEdit] = None
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 stylesheet_path = os.path.join(script_dir, "stylesheet.css")
 
 window = None
+question_window = None
 
 with open(stylesheet_path, 'r') as f:
     stylesheet = f.read()
@@ -43,11 +45,28 @@ def handle_login():
     window.show()
 
 def handle_login_clicked():
-    global email_textbox, password_textbox
-    email = email_textbox.text()
-    password = password_textbox.text()
-    main.login(email, password)
+    global email_textbox, password_textbox, window
+    email = email_textbox.text()        #type:ignore
+    password = password_textbox.text()  #type:ignore
+    questions = main.second_auth(email, password) #type:ignore
+    window.close() #type:ignore
+    question_window = QMainWindow()
+    question_window.setWindowTitle("Login to WrapperDirecte")
+    question_window.setGeometry(100,100,500,500)
     
+    main_widget = QWidget()
+    main_layout = QVBoxLayout()
+    
+    main_widget.setLayout(main_layout)
+    
+    question = questions[0]
+    question_label = QLabel(question)
+    
+    responses = QComboBox()
+    responses.addItems(questions[1])
+    
+    main_layout.addWidget(question_label)
+    main_layout.addWidget(responses)
 
 def main_loop():
     app = QApplication(sys.argv)
