afbff400173b7f0a405a24a0f5943fc9b70936c3
HenriCaboche <henri.caboche.develop@gmail.com>
2025-07-15 21:23:25 +0200

    First login screen stub

--- gui/main_gui.py
+++ gui/main_gui.py
@@ -1,11 +1,34 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QWidget, QLineEdit
 
-app = QApplication(sys.argv)
+window = None
 
-window = QMainWindow()
-window.setWindowTitle("WrapperDirecte")
-window.setGeometry(100, 100, 800, 600)  
-window.show()
+def handle_login():
+    global window
+    window = QMainWindow()
+    window.setWindowTitle("Login in WrapperDirecte")
+    window.setGeometry(100, 100, 800, 600)
+    
+    main_widget = QWidget()
+    main_layout = QVBoxLayout()
+    
+    window.setCentralWidget(main_widget)
+    
+    email_textbox = QLineEdit(placeholderText="Email")
+    password_textbox = QLineEdit(placeholderText= "Password")
+    
+    main_layout.addWidget(email_textbox)
+    main_layout.addWidget(password_textbox)
+    main_widget.setLayout(main_layout)
 
-sys.exit(app.exec_())  
+    window.show()
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    handle_login()
+
+    sys.exit(app.exec())  
+
+main()
